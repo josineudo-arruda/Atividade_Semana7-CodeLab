41a9080dbd86d1e3406462ad6e8a9b8f1f79a10d
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-05-25 16:33:14 -0300

    feat: add windstrom details

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Hero } from "../hero";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
