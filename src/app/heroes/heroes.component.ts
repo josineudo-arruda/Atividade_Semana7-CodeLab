5ed8a321c3fdcd3062dae67e8b59b8cd78c024ab
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-05-25 16:36:36 -0300

    feat: add style in list of heroes display

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { Hero } from "../hero";
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
